80974bf9db92215a5226e208548d7756ff40ae5b
Pruthviraj-MV <74802690+pruthvirajmv@users.noreply.github.com>
2021-05-24 17:43:25 +0530

    fix: functionalities issue

--- models/likedVideo.model.js
+++ models/likedVideo.model.js
@@ -1,21 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const LikedVideoSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+  videos: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
     },
+  ],
+});
 
-    videos: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Video'
-    }]
+const LikedVideo = mongoose.model("LikedVideo", LikedVideoSchema);
 
-})
-
-const LikedVideos = mongoose.model('LikedVideos', LikedVideoSchema);
-
-module.exports = {
-    LikedVideos
-};
+module.exports = { LikedVideo };
